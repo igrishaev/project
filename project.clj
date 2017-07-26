dda336c92543d8f892fcf8b652cb2cbfbcebefcd
Ivan Grishaev <ivan@grishaev.me>
2017-07-26 18:48:40 +0300

    back to postgres

--- project.clj
+++ project.clj
@@ -4,20 +4,22 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :repositories {"my.datomic.com"
-                 {:url "https://my.datomic.com/repo"
-                  :creds :gpg}}
-
   :plugins [[migratus-lein "0.5.0"]
             [lein-environ "1.1.0"]]
 
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [mount "0.1.11"]
-                 [com.datomic/datomic-pro "0.9.5561.50"]
+                 [conman "0.6.7" :exclusions [org.slf4j/slf4j-api]]
+                 [org.postgresql/postgresql "42.1.3"]
+                 [migratus "0.9.8"]
                  [environ "1.1.0"]
                  [clj-time "0.14.0"]
                  [clj-http "3.6.1"]
                  [compojure "1.6.0"]
                  [ring/ring-jetty-adapter "1.6.2"]
-                 [ring/ring-json "0.4.0"]])
+                 [ring/ring-json "0.4.0"]]
+
+  :migratus {:store :database
+             :migration-dir "migrations"
+             :db ~(get (System/getenv) "DATABASE_URL")}))
