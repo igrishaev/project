d6e5102cd65f437578b4b9238ffabd4c9ffd46be
Ivan Grishaev <ivan@grishaev.me>
2017-07-30 14:38:07 +0300

    23 http CM

--- src/project/http.clj
+++ src/project/http.clj
@@ -1,13 +1,43 @@
 (ns project.http
-  (:require [clj-http.client :as client])
+  (:require [clj-http.client :as client]
+            [project.conf :refer [conf]]
+            [clj-http.conn-mgr
+             :refer [make-reusable-conn-manager
+                     shutdown-manager]]
+            [mount.core :as mount])
   (:refer-clojure :exclude [get]))
+
+(declare CM)
+
+(defn- on-start []
+  (let [params {:timeout (:http-timeout conf)
+                :threads (:http-threads conf)}]
+    (make-reusable-conn-manager params)))
+
+(defn- on-stop []
+  (shutdown-manager CM))
+
+(mount/defstate CM
+  :start (on-start)
+  :stop (on-stop))
+
+(defn start! []
+  (mount/start #'CM))
+
+(defn stop! []
+  (mount/stop #'CM))
+
+;; todo exception
 
 (defn request [method url & [params]]
   (client/request
-   (assoc params
-          :method method
-          :throw-exceptions true
-          :url url)))
+   (-> params
+       (assoc :method method
+              :connection-manager CM
+              :throw-exceptions true
+              :url url)
+       (assoc-in [:headers "User-Agent"]
+                 (:http-user-agent conf)))))
 
 (def get (partial request :get))
 
