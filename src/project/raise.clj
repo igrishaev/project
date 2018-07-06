72ff02e30b1c3b8ac2f011a8daed80fd7724169e
Ivan Grishaev <ivan@grishaev.me>
2018-07-06 12:21:22 +0300

    unused files removed

--- src/project/raise.clj
+++
@@ -1,5 +0,0 @@
-(ns project.raise
-  (:require [clojure.string :as str]))
-
-(defn raise [& args]
-  (throw (Exception. (str/join \space args))))
--- src/project/uri.clj
+++
@@ -1,8 +0,0 @@
-(ns project.uri
-  (:import java.net.URI))
-
-(defn read-uri [value]
-  (URI. value))
-
-(defn is-uri? [value]
-  (instance? URI value))
