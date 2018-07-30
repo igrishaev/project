4798e26ceb2bfbd2ba7a31649f47634cdc19b9d4
Ivan Grishaev <ivan@grishaev.me>
2018-07-30 23:35:25 +0300

    ua fix

--- src/project/search.clj
+++ src/project/search.clj
@@ -108,9 +108,14 @@
     [(:id feed)]))
 
 
+(def user-agent
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
+
+
 (def http-opt
   {:as :stream
-   :throw-exceptions true})
+   :throw-exceptions true
+   :headers {"User-Agent" user-agent}})
 
 
 (defn fetch-url
