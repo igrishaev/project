c2be97c005c91a9c927a051086306641c0deed49
Ivan Grishaev <ivan@grishaev.me>
2018-08-05 15:20:57 +0300

    no more conn manager

--- src/project/http.clj
+++ src/project/http.clj
@@ -1,25 +1,18 @@
 (ns project.http
-  (:require [clj-http.client :as client]
-            [clj-http.conn-mgr
-             :refer [make-reusable-conn-manager
-                     shutdown-manager]])
+  (:require [clj-http.client :as client])
   (:refer-clojure :exclude [get]))
 
 
 (def user-agent
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
 
-
-(def conn-manager
-  (make-reusable-conn-manager
-   {:timeout 5
-    :threads 4
-    :insecure? true}))
-
+(def timeout (* 1000 10))
 
 (def opt-default
   {:throw-exceptions true
-   :connection-manager conn-manager
+   :insecure? true
+   :socket-timeout timeout
+   :conn-timeout timeout
    :headers {"User-Agent" user-agent}})
 
 
